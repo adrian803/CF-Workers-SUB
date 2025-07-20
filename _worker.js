db854005799e8fa9d5928759ff9a15644aea2820
菜鸡林某 <69416558+adrian803@users.noreply.github.com>
2025-07-20 23:27:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,12 +14,13 @@ let cacheTTL = 24 ;//小时，缓存时长
 
 //节点链接 + 订阅链接
 let MainData = `
-https://sub3.smallstrawberry.com/api/v1/client/subscribe?token=txk14gaiefdajkjytuagqu5pnb0pp4le
 http://s.hajimi.icu/v3/subscr?id=ac424a8637304d46b119171d0d541aeb
-https://4.feiniaoyun01.life/api/v1/client/subscribe?token=029a3d974b503f71d5615fd4907c3252
-http://nnm.dingyue.cdn.5188801.xyz/api/v1/client/subscribe?token=8e8b5ce95a69cf885e947b4426e2f8b8
-https://sub-01.suyunsub.top/api/v1/client/subscribe?token=93fb58e9be9bdf855eccede7237b3ed5
-https://xxx.qianxin.icu/123789456l?sub
+https://sub.suyunsub.top/suyun/v1/client/subscribe?token=93fb58e9be9bdf855eccede7237b3ed5
+https://nnm.zvmrnh.209283.xyz/nannanmiao?token=8e8b5ce95a69cf885e947b4426e2f8b8&flag=meta
+https://f1.fnxx.cc/api/v1/client/subscribe?token=029a3d974b503f71d5615fd4907c3252
+https://xn--cp3a08l.com/api/v1/client/subscribe?token=919d7e22ffd058179ebdfb0f84758a7d&flag=meta
+https://sub1.smallstrawberry.com/api/v1/client/subscribe?token=txk14gaiefdajkjytuagqu5pnb0pp4le
+https://xxx.linxi.online/123789456l?sub
 `
 
 let urls = [];
